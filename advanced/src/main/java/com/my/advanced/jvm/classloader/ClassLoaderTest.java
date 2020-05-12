27a9572832b8905e11c375ac5ebb4fc8e2560708
mayue <374176008@qq.com>
2020-05-12 11:33:53 +0800

    1. 添加深拷贝与浅拷贝

---
+++ advanced/src/main/java/com/my/advanced/jvm/classloader/ClassLoaderTest.java
@@ -0,0 +1,39 @@
+package com.my.advanced.jvm.classloader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 类加载器与 instance of 关键字演示
+ * 不同的类加载器对 instance of 关键字运算的结果的影响
+ *
+ * @author YMa69
+ * @date 2019/12/2
+ */
+public class ClassLoaderTest {
+
+    public static void main(String[] args) throws Exception{
+        ClassLoader myLoader = new ClassLoader() {
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                try{
+                    String fileName = name.substring(name.lastIndexOf(".")+1) + ".class";
+                    InputStream is = getClass().getResourceAsStream(fileName);
+                    if(is==null){
+                        return super.loadClass(name);
+                    }
+                    byte[] b = new byte[is.available()];
+                    is.read(b);
+                    return defineClass(name, b, 0, b.length);
+                }catch (IOException e){
+                    throw new ClassNotFoundException(name);
+                }
+            }
+        };
+
+        Object obj = myLoader.loadClass("com.my.advanced.jvm.classloader.ClassLoaderTest").newInstance();
+
+        System.out.println(obj.getClass());
+        System.out.println(obj instanceof com.my.advanced.jvm.classloader.ClassLoaderTest);
+    }
+}
